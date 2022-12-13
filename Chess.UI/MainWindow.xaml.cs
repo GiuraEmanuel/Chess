07d16ebf77b4b6da0f390cfd81ae36b31f65b8c3
Giura Emanuel <emanuel_sb_96@yahoo.com>
2022-12-13 22:37:00 +0200

    Create chess board

--- Chess.UI/MainWindow.xaml.cs
+++ Chess.UI/MainWindow.xaml.cs
@@ -1,17 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Chess.Engine;
+using System;
+using System.Drawing;
+using System.Reflection;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Color = System.Windows.Media.Color;
+using Size = System.Drawing.Size;
 
 namespace Chess.UI
 {
@@ -23,6 +18,42 @@ namespace Chess.UI
         public MainWindow()
         {
             InitializeComponent();
+            DrawChessBoard();
+        }
+
+        public void DrawChessBoard()
+        {
+            int totalRows = 8;
+            int totalColumns = 8;
+
+            for (int row = 0; row < totalRows; row++)
+            {
+                BoardGrid.ColumnDefinitions.Add(new ColumnDefinition());
+                BoardGrid.RowDefinitions.Add(new RowDefinition());
+
+                for (int column = 0; column < totalColumns; column++)
+                {
+                    var border = new Border
+                    {
+                        Height = 50,
+                        Width = 50,
+                        BorderThickness = new Thickness(1, 1, 1, 1),
+                         
+                    };
+                    BoardGrid.Children.Add(border);
+                    Grid.SetColumn(border, column);
+                    Grid.SetRow(border, row);
+
+                    if ((row + column) % 2 == 0)
+                    {
+                        border.Background = Brushes.White;
+                    }
+                    else
+                    {
+                        border.Background = Brushes.Black;
+                    }
+                }
+            }
         }
     }
 }
